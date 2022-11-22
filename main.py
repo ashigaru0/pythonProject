097dc013bfd68060f7571c2a43dc0c927aeac7b7
ashigaru0 <ashigarua0@gmail.com>
2022-11-22 16:40:42 +0700

    done

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
 import sys
+import random
 
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPainter, QColor
 
 
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('UI.ui', self)
-        self.pushButton.clicked.connect(self.run)
+        self.setFixedSize(500, 500)
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
 
-    def run(self):
-        pass
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw(qp)
+            qp.end()
+            self.do_paint = False
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def draw(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        size = random.randint(0, 200)
+        qp.drawEllipse(random.randint(0, 500), random.randint(0, 500), size, size)
 
 
 if __name__ == '__main__':
